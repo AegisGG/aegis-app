05801adff61467675f2419d2d4e078df21d9e36a
AegisGG <116173055+AegisGG@users.noreply.github.com>
2022-10-29 14:39:11 +0800

    update: guide steps

--- data/guide-steps.js
+++ data/guide-steps.js
@@ -1,6 +1,6 @@
 const guideSteps = [
   'Connect wallet',
-  'Press Enable Skating to allow Staking of AEGIS tokens',
+  'Press Enable Staking to allow Staking of AEGIS tokens',
   'Approve the Allowance Pop-up',
   'Input desired amount of AEGIS tokens or press the Max button to wager',
   'There is a 10% fee when participating on Wagers',
